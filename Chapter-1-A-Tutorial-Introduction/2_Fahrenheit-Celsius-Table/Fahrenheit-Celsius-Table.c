4af761d64014a6b3013c3638fb09f08e74284d92
Al <alexxmatei@gmail.com>
2021-01-02 12:42:45 +0200

    Updated program to respect MISRA & Static code check rules.

--- Chapter-1-A-Tutorial-Introduction/2_Fahrenheit-Celsius-Table/Fahrenheit-Celsius-Table.c
+++ Chapter-1-A-Tutorial-Introduction/2_Fahrenheit-Celsius-Table/Fahrenheit-Celsius-Table.c
@@ -7,15 +7,29 @@
 
 #include <stdio.h>
 
-#define LOWER 0      /* lower limit of the table */
-#define UPPER 300    /* upper limit */
-#define STEP  20     /* step size */
+#define LOWER  0      /* lower limit of the table */
+#define UPPER  300    /* upper limit */
+#define STEP   20     /* step size */
+#define F5_K   5.0f
+#define F9_K   9.0f
+#define S32_K  32
+
+float fahrToCelsius(int fahr);
 
 /* print Fahrenheit-Celsius table */
-main()
+int main()
 {
 	int fahr;
 
 	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+	{
+		(void)printf("%3d %6.1f\n", fahr, fahrToCelsius(fahr));
+	}
+
+	return 0;
+}
+
+float fahrToCelsius(int fahr)
+{
+	return F5_K / F9_K * (fahr - S32_K);
 }

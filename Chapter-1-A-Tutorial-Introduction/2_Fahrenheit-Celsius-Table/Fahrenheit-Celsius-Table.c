2045d7d8298ee6db8579ee162a4c5570491ed80f
Al <alexxmatei@gmail.com>
2021-01-02 11:25:52 +0200

    Updated the program to the final version presented in the book.

--- Chapter-1-A-Tutorial-Introduction/2_Fahrenheit-Celsius-Table/Fahrenheit-Celsius-Table.c
+++ Chapter-1-A-Tutorial-Introduction/2_Fahrenheit-Celsius-Table/Fahrenheit-Celsius-Table.c
@@ -7,11 +7,15 @@
 
 #include <stdio.h>
 
-/* print Fahrenheit-Celsius table in reverse order */
+#define LOWER 0      /* lower limit of the table */
+#define UPPER 300    /* upper limit */
+#define STEP  20     /* step size */
+
+/* print Fahrenheit-Celsius table */
 main()
 {
 	int fahr;
 
-	for (fahr = 300; fahr >= 0; fahr = fahr - 20)
-		printf("%3d %6.1f\n", fahr, (5.0 / 9.0)*(fahr-32));
+	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }

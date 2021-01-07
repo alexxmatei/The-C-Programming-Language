5a7c4dab91740d494cbe469459ae5bc13e47bb6f
Al <alexxmatei@gmail.com>
2021-01-07 08:38:11 +0200

    Initial commit of exercise 1-7 from the K&R book.

---
+++ Chapter-1-A-Tutorial-Introduction/3_CopyInputToOutput/Exercise_1-7.c
@@ -0,0 +1,17 @@
+/*
+ * Exercise_1-7.c
+ *
+ *  Created on: 7 Jan 2021
+ *      Author: Al
+ *
+ *  Exercise: Write a program to print the value of EOF.
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	(void)printf("The value of EOF is: %d",EOF);
+
+	return 0;
+}

01b5e6c0df0d326ee18af0edd9da468188587c92
Al <alexxmatei@gmail.com>
2021-01-15 17:35:21 +0200

    Fixed all gcc warnings and some MISRA issues.

--- Chapter-1-A-Tutorial-Introduction/3_CopyInputToOutput/CopyInputToOutput.c
+++ Chapter-1-A-Tutorial-Introduction/3_CopyInputToOutput/CopyInputToOutput.c
@@ -8,10 +8,14 @@
 #include <stdio.h>
 
 /* copy input to output; 2nd version */
-main()
+int main()
 {
 	int c;
 
-	while ((c = getchar()) != EOF)
-		putchar(c);
+	while ( ( c = getchar() ) != EOF )
+	{
+		(void)putchar(c);
+	}
+
+	return 0;
 }

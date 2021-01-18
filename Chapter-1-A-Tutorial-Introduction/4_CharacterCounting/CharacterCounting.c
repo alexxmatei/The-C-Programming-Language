2995aae2f365770401ed3f6a3b7a7db1a8afc036
Al <alexxmatei@gmail.com>
2021-01-18 18:15:33 +0200

    Fixed all gcc warnings and some MISRA issues.

--- Chapter-1-A-Tutorial-Introduction/4_CharacterCounting/CharacterCounting.c
+++ Chapter-1-A-Tutorial-Introduction/4_CharacterCounting/CharacterCounting.c
@@ -8,11 +8,15 @@
 #include <stdio.h>
 
 /* count characters in input; 2nd version */
-main()
+int main()
 {
 	double nc;
 
 	for (nc = 0; getchar () != EOF; ++nc)
+	{
 		;
-	printf("%.0f\n", nc);
+	}
+	(void)printf("%.0f\n", nc);
+
+	return 0;
 }

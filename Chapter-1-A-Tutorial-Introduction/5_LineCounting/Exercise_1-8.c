e0432a21947bd903eea72bbb107c545fd1c52f38
Al <alexxmatei@gmail.com>
2021-01-26 10:32:12 +0200

    Initial commit of exercise 1-8 from the K&R book.

---
+++ Chapter-1-A-Tutorial-Introduction/5_LineCounting/Exercise_1-8.c
@@ -0,0 +1,35 @@
+/*
+ * Exercise_1-8.c
+ *
+ *  Created on: 26 Jan 2021
+ *      Author: Al
+ */
+
+#include <stdio.h>
+
+/* count blanks, tabs, and newlines in input */
+int main()
+{
+	int c;
+	int b = 0;
+	int t = 0;
+	int nl = 0;
+
+	while ( ( c = getchar() ) != EOF )
+	{
+		if ( c == ' ' )
+		{
+			++b;
+		}
+		if ( c == '\t' )
+		{
+			++t;
+		}
+		if ( c == '\n' )
+		{
+			++nl;
+		}
+	}
+	(void)printf("\nNumber of blanks: %d\nNumber of tabs: %d\nNumber of newlines: %d\n", b, t, nl);
+	return 0;
+}

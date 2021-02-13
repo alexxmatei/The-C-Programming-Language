25588027fed9833cf31becab70f09cf65c164324
Alexandru Matei <alexxmatei@gmail.com>
2021-02-13 11:59:18 +0200

    Added outputting of ocurrences for words of different lengths.

--- Chapter-1-A-Tutorial-Introduction/7_Arrays/Exercise_1-13.c
+++ Chapter-1-A-Tutorial-Introduction/7_Arrays/Exercise_1-13.c
@@ -9,13 +9,28 @@
 
 #define IN  1  /* inside a word */
 #define OUT 0  /* outside a word */
+#define MAX_WORD_LEN 99 /* maximum word length */
+#define INDEX_WORD_LEN (i + 1)
 
 /* prints a histogram of the lengths of words in input */
+/* ToDo Optimise code if a good solution is found;
+ * having to re-iterate through the whole array every time an action is performed takes a lot of time */
 int main()
 {
 	int c;
 	int state = IN;
 	int wl = 0; /* word length */
+	int histogram[MAX_WORD_LEN]; /* ToDo Initialise at declaration */
+
+	/* initialise the array */
+	for (int i = 0; i < MAX_WORD_LEN; i++) { /* ToDo why does i <= MAX_WORD_LEN (with value 1000) create an infinite loop? */
+		histogram[i] = 0;
+		/* debug begin */
+		/* ToDo why does getchar() input start before console outputting is finished for MAX_WORD_LEN = 1000?
+		 *      why does getchar() input start before console outputting is started  for MAX_WORD_LEN =   99? */
+		(void)printf("histogram[%d] = %d\n", i, histogram[i]);
+		/* debug finish */
+	}
 
 	for ( c = getchar(); c != EOF; c = getchar() ) {
 		/* not a word, either a blank, tab or newline */
@@ -23,6 +38,12 @@ int main()
 			/* if previous character was the end of a word */
 			if ( state == IN ) {
 				(void)printf("Word length is: %d\n", wl);
+
+				for (int i = 0; i < MAX_WORD_LEN; i++) {
+					if( wl == INDEX_WORD_LEN ) {
+						++histogram[i];
+					}
+				}
 			}
 			state = OUT;
 		}
@@ -37,4 +58,9 @@ int main()
 		}
 	}
 
+	for (int i = 0; i < MAX_WORD_LEN; i++) {
+		if ( histogram[i] != 0 ) {
+			(void)printf("Occurrences of %2d letter words: %d\n", INDEX_WORD_LEN, histogram[i]);
+		}
+	}
 }

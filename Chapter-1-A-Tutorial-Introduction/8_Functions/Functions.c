1693785c84813bccb31ac3cccaeb9560ba0063b9
Alexandru Matei <alexxmatei@gmail.com>
2021-02-19 05:28:58 +0200

    Fixed all GCC and Codan warnings as well as most MISRA issues.

--- Chapter-1-A-Tutorial-Introduction/8_Functions/Functions.c
+++ Chapter-1-A-Tutorial-Introduction/8_Functions/Functions.c
@@ -7,25 +7,33 @@
 
 #include <stdio.h>
 
+#define S2_K  2
+#define S3_K  3
+#define N3_K  -3
+#define S10_K 10
+
 int power(int m, int n);
 
 /* test power function */
-main()
+int main()
 {
 	int i;
 
-	for (i = 0; i < 10; ++i)
-		printf("%d %d %d\n", i, power(2,i), power(-3,i));
+	for (i = 0; i < S10_K; ++i) {
+		(void)printf("%d %d %d\n", i, power(S2_K,i), power(N3_K,i));
+	}
 	return 0;
 }
 
 /* power: raise base to n-th power; n >= 0 */
 int power(int base, int n)
 {
-	int i, p;
+	int i;
+	int p;
 
 	p = 1;
-	for (i = 1; i <= n; ++i)
+	for (i = 1; i <= n; ++i) {
 		p = p * base;
+	}
 	return p;
 }

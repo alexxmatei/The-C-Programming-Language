ff4c8981c34b1004d6da988f59440bf3323b68c9
Alexandru Matei <alexxmatei@gmail.com>
2021-03-31 08:23:39 +0300

    Uncommented a portion of the old code, responsible for drawing the histogram.
    The histogram now draws the number of character occurrences instead of word lengths.

--- Chapter-1-A-Tutorial-Introduction/7_Arrays/Exercise_1-14.c
+++ Chapter-1-A-Tutorial-Introduction/7_Arrays/Exercise_1-14.c
@@ -14,18 +14,18 @@ typedef unsigned char bool;
 #define FALSE (bool)0u
 
 #define MAX_CHARACTERS_TRACKED 26 /* maximum word length tracked by the histogram */
-//#define HIST_HEIGHT  20 /* height of the histogram */
-//#define DECIMAL_PRECISION 2 /* the number of decimals used for the percentages */
-//#define MIN_DECIMAL_VAL (1 / pow(10, DECIMAL_PRECISION)) /* get minimum decimal value based on precision */
-//#define MAX_PERCENTAGE 100
-//#define MAX_NR_WIDTH (int)log10(MAX_PERCENTAGE) + 1 /* obtain the max number width based on the widest number, the maximum percentage */
+#define HIST_HEIGHT  20 /* height of the histogram */
+#define DECIMAL_PRECISION 2 /* the number of decimals used for the percentages */
+#define MIN_DECIMAL_VAL (1 / pow(10, DECIMAL_PRECISION)) /* get minimum decimal value based on precision */
+#define MAX_PERCENTAGE 100
+#define MAX_NR_WIDTH (int)log10(MAX_PERCENTAGE) + 1 /* obtain the max number width based on the widest number, the maximum percentage */
 //#define MAX_OCCURRENCES_INDEX_NR 99 /* the maximum number to be displayed as an index under the histogram */
 //#define S10_C 10 /* signed value constant */
 
 /* printf format defines */
-//#define PRINTF_NR_1_WIDTH (DECIMAL_PRECISION + MAX_NR_WIDTH + 1) /* the number plus the '.' */
+#define PRINTF_NR_1_WIDTH (DECIMAL_PRECISION + MAX_NR_WIDTH + 1) /* the number plus the '.' */
 //#define CHARS_BETWEEN_NR1_NR2 4 /* the number of characters between the 2 numbers in the printf format string: "% - " */
-//#define PRINTF_NR_2_WIDTH (DECIMAL_PRECISION + MAX_NR_WIDTH + 1) /* the number plus the '.' */
+#define PRINTF_NR_2_WIDTH (DECIMAL_PRECISION + MAX_NR_WIDTH + 1) /* the number plus the '.' */
 //#define CHARS_AFTER_NR2 3 /* the number of characters after the 2nd number in the printf format string: "%: " */
 //#define PRINTF_TEXT_LEN PRINTF_NR_1_WIDTH + CHARS_BETWEEN_NR1_NR2 + PRINTF_NR_2_WIDTH + CHARS_AFTER_NR2
 
@@ -34,7 +34,7 @@ typedef unsigned char bool;
 int main()
 {
 	int c;
-	//int max = 0; /* maximum occurrence of word */
+	int max = 0; /* maximum occurrence of word */
 	int histogram[MAX_CHARACTERS_TRACKED] = { 0 }; /* initialise the array with 0 */
 	/* initialise the array with non character values (-1) */
 	/* note: characters[MAX_CHARACTERS_TRACKED] = { 0 } only works with 0 */
@@ -70,69 +70,69 @@ int main()
 		}
 	}
 
-//	/* find the maximum occurrences of a word */
-//	for ( int i = 0; i < MAX_CHARACTERS_TRACKED; ++i ) {
-//		if ( max < histogram[i] ) {
-//			max = histogram[i];
-//		}
-//	}
-//
-//	{
-//		int prc[HIST_HEIGHT] = { 0 };
-//		/* go through the top of the histogram to the bottom */
-//		for ( int i = HIST_HEIGHT; i >= 0; --i ) {
-//			if ( i != 0 ) {
-///* ignore -Wconversion for the line between the #pragma statements */
-///* Justification: the conversion is intended */
-//#pragma GCC diagnostic push /* remember state of the diagnostics before changes */
-//#pragma GCC diagnostic ignored "-Wconversion"
-//				prc[i - 1] = (int)((float)i / HIST_HEIGHT * MAX_PERCENTAGE);
-//#pragma GCC diagnostic pop /* restore state of the diagnostics before changes */
-//			}
-//			if ( i != HIST_HEIGHT ) { /* skip the first and highest value, because we add an extra row at the end */
-//				/* if this is the first row */
-//				if ( prc[i] == MAX_PERCENTAGE ) {
-//					(void)printf("%*.*f%% - %*.*f%%: ",
-//								 PRINTF_NR_1_WIDTH, DECIMAL_PRECISION, (float)prc[i - 1],
-//								 PRINTF_NR_2_WIDTH, DECIMAL_PRECISION, (float)prc[i]);
-//				}
-//				/* else if this is the last row */
-//				else if ( i == 0 ) {
-//					(void)printf("%*.*f%% - %*.*f%%: ",
-//								 PRINTF_NR_1_WIDTH, DECIMAL_PRECISION, MIN_DECIMAL_VAL,
-//								 PRINTF_NR_2_WIDTH, DECIMAL_PRECISION, prc[i] - MIN_DECIMAL_VAL);
-//				}
-//				else {
-//					(void)printf("%*.*f%% - %*.*f%%: ",
-//								 PRINTF_NR_1_WIDTH, DECIMAL_PRECISION, (float)prc[i - 1],
-//								 PRINTF_NR_2_WIDTH, DECIMAL_PRECISION, prc[i] - MIN_DECIMAL_VAL);
-//				}
-//				for ( int j = 0; j < MAX_CHARACTERS_TRACKED; ++j ) {
-//					float ref = (float)i; /* reference */
-//					if ( i == 0 ) {
-//						/* instead of 0 take the minimum decimal value as reference */
-//						/* this way values with 0 occurrences will be ignored,
-//						 * they will not be drawn as a '#' symbol in the last row */
-//						ref = (float)MIN_DECIMAL_VAL;
-//					}
-//					/* the percentage of occurrences stored at the current index
-//					 * relative to the maximum number of occurrences */
-//					float prcOfMax = (float)histogram[j] / (float)max;
-//					/* scale the percentage (rounded down)
-//					 * based on the height of the histogram */
-//					float scaledPrc = (prcOfMax * (float)HIST_HEIGHT);
-//					/* draw the histogram symbols for each row/column */
-//					if ( scaledPrc >= ref ) {
-//						(void)printf("%-3c", '#');
-//					}
-//					else {
-//						(void)printf("%-3c", '·');
-//					}
-//				}
-//			}
-//			(void)putchar('\n');
-//		}
-//	}
+	/* find the maximum occurrences of a word */
+	for ( int i = 0; i < MAX_CHARACTERS_TRACKED; ++i ) {
+		if ( max < histogram[i] ) {
+			max = histogram[i];
+		}
+	}
+
+	{
+		int prc[HIST_HEIGHT] = { 0 };
+		/* go through the top of the histogram to the bottom */
+		for ( int i = HIST_HEIGHT; i >= 0; --i ) {
+			if ( i != 0 ) {
+/* ignore -Wconversion for the line between the #pragma statements */
+/* Justification: the conversion is intended */
+#pragma GCC diagnostic push /* remember state of the diagnostics before changes */
+#pragma GCC diagnostic ignored "-Wconversion"
+				prc[i - 1] = (int)((float)i / HIST_HEIGHT * MAX_PERCENTAGE);
+#pragma GCC diagnostic pop /* restore state of the diagnostics before changes */
+			}
+			if ( i != HIST_HEIGHT ) { /* skip the first and highest value, because we add an extra row at the end */
+				/* if this is the first row */
+				if ( prc[i] == MAX_PERCENTAGE ) {
+					(void)printf("%*.*f%% - %*.*f%%: ",
+								 PRINTF_NR_1_WIDTH, DECIMAL_PRECISION, (float)prc[i - 1],
+								 PRINTF_NR_2_WIDTH, DECIMAL_PRECISION, (float)prc[i]);
+				}
+				/* else if this is the last row */
+				else if ( i == 0 ) {
+					(void)printf("%*.*f%% - %*.*f%%: ",
+								 PRINTF_NR_1_WIDTH, DECIMAL_PRECISION, MIN_DECIMAL_VAL,
+								 PRINTF_NR_2_WIDTH, DECIMAL_PRECISION, prc[i] - MIN_DECIMAL_VAL);
+				}
+				else {
+					(void)printf("%*.*f%% - %*.*f%%: ",
+								 PRINTF_NR_1_WIDTH, DECIMAL_PRECISION, (float)prc[i - 1],
+								 PRINTF_NR_2_WIDTH, DECIMAL_PRECISION, prc[i] - MIN_DECIMAL_VAL);
+				}
+				for ( int j = 0; j < MAX_CHARACTERS_TRACKED; ++j ) {
+					float ref = (float)i; /* reference */
+					if ( i == 0 ) {
+						/* instead of 0 take the minimum decimal value as reference */
+						/* this way values with 0 occurrences will be ignored,
+						 * they will not be drawn as a '#' symbol in the last row */
+						ref = (float)MIN_DECIMAL_VAL;
+					}
+					/* the percentage of occurrences stored at the current index
+					 * relative to the maximum number of occurrences */
+					float prcOfMax = (float)histogram[j] / (float)max;
+					/* scale the percentage (rounded down)
+					 * based on the height of the histogram */
+					float scaledPrc = (prcOfMax * (float)HIST_HEIGHT);
+					/* draw the histogram symbols for each row/column */
+					if ( scaledPrc >= ref ) {
+						(void)printf("%-3c", '#');
+					}
+					else {
+						(void)printf("%-3c", '·');
+					}
+				}
+			}
+			(void)putchar('\n');
+		}
+	}
 //
 //	/* print spaces equal to the width of the percentage row
 //	 * this is so that the indexes are printed at the correct positions */

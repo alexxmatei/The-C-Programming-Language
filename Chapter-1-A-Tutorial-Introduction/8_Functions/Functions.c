713d308343b0d4742ff5725589be7494eff2d956
Alexandru Matei <alexxmatei@gmail.com>
2021-04-15 21:26:17 +0300

    Updated program with the second version from the K&R book.

--- Chapter-1-A-Tutorial-Introduction/8_Functions/Functions.c
+++ Chapter-1-A-Tutorial-Introduction/8_Functions/Functions.c
@@ -25,14 +25,12 @@ int main()
 	return 0;
 }
 
-/* power: raise base to n-th power; n >= 0 */
+/* power: raise base to n-th power; n >= 0; version 2 */
 int power(int base, int n)
 {
-	int i;
 	int p;
 
-	p = 1;
-	for (i = 1; i <= n; ++i) {
+	for (p = 1; n > 0; --n) {
 		p = p * base;
 	}
 	return p;

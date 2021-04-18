c9314b50e2dcff70a040ea82e6d72420ada5552f
Alexandru Matei <alexxmatei@gmail.com>
2021-04-18 07:57:36 +0300

    Fixed all GCC warnings as well as most MISRA issues.

--- Chapter-1-A-Tutorial-Introduction/9_CharacterArrays/CharacterArrays.c
+++ Chapter-1-A-Tutorial-Introduction/9_CharacterArrays/CharacterArrays.c
@@ -12,7 +12,7 @@ int getsline(char line[], int maxline);
 void copy(char to[], char from[]);
 
 /* print longest input line */
-main()
+int main()
 {
 	int len;				/* current line length */
 	int max;				/* maximum length seen so far */
@@ -20,23 +20,27 @@ main()
 	char longest[MAXLINE];	/* longest line saved here */
 
 	max = 0;
-	while ( (len = getsline(line, MAXLINE)) > 0 )
+	for ( len = getsline(line, MAXLINE); len > 0; len = getsline(line, MAXLINE) ) {
 		if ( len > max ) {
 			max = len;
 			copy(longest, line);
 		}
-	if ( max > 0 ) /* there was a line */
-		printf("%s", longest);
+	}
+	if ( max > 0 ) { /* there was a line */
+		(void)printf("%s", longest);
+	}
 	return 0;
 }
 
 /* getsline: read a line into s, return length */
 int getsline(char s[], int lim)
 {
-	int c, i;
+	int c;
+	int i;
 
-	for ( i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i )
+	for ( i = 0; (i < (lim - 1)) && ((c = getchar()) != EOF) && (c != '\n'); ++i ) {
 		s[i] = c;
+	}
 	if ( c == '\n' ) {
 		s[i] = c;
 		++i;
@@ -51,6 +55,7 @@ void copy(char to[], char from[])
 	int i;
 
 	i = 0;
-	while ( (to[i] = from[i]) != '\0' )
+	for ( to[i] = from[i]; to[i] != '\0'; to[i] = from[i] ) {
 		++i;
+	}
 }
